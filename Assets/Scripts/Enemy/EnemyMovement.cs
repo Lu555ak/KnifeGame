10d6fc7e0fb15955e5300833563845151ebd9b21
Lxoy <lo14loyt@gmail.com>
2022-06-15 22:24:05 +0200

    Added awareness bar

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class EnemyMovement : MonoBehaviour
 {
@@ -28,8 +29,9 @@ public class EnemyMovement : MonoBehaviour
     private float inLongSightMultiplier = 0.75f;
     private float notInSight = 0.1f;
     private float inSight = 0.2f;
-    private float cooldown = 1f;
+    private float cooldown = 0.5f;
     private float cooldownTimer = 0f;
+    public Image bar;
 
 
     private void Awake()
@@ -52,38 +54,53 @@ public class EnemyMovement : MonoBehaviour
 
         if (!playerInNearSightRange && !playerInLongSightRange)
         {
-            Patroling();
             if (Time.time > cooldownTimer)
             {
                 inSightMeter -= notInSight;
                 cooldownTimer = Time.time + cooldown;
             }
+            Patroling();
         }
 
         if (playerInNearSightRange && !playerInLongSightRange)
         {
-            ChasePlayer();
             if (Time.time > cooldownTimer)
             {
                 inSightMeter += inSight * inNearSightMultiplier;
                 cooldownTimer = Time.time + cooldown;
             }
+            ChasePlayer();
         }
 
         if (playerInNearSightRange && playerInLongSightRange)
         {
-            SpotPlayer();
             if (Time.time > cooldownTimer)
             {
                 inSightMeter += inSight * inLongSightMultiplier;
                 cooldownTimer = Time.time + cooldown;
             }
+            SpotPlayer();
         }
 
         if(inSightMeter >= 1f)
         {
             Debug.Log(":)");
         }
+
+        if(inSightMeter >= 0.76f)
+        {
+            bar.color = new Color32(255, 0, 0, 255);
+        }
+        else if(inSightMeter >= 0.51 && inSightMeter <= 0.75f)
+        {
+            bar.color = new Color32(255, 255, 0, 255);
+        }
+        else
+        {
+            bar.color = new Color32(0, 128, 0, 255);
+        }
+
+        bar.fillAmount = inSightMeter;
     }
 
     private void Patroling()
